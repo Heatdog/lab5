c71393c6a12fa7f62902266223890ff23d7a1eb0
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-05-07 19:03:54 +0300

    Create dialog.h

---
+++ dialog.h
@@ -0,0 +1,16 @@
+//
+// Created by yura on 07.05.2021.
+//
+
+#ifndef LAB5_SEM2_DIALOG_H
+#define LAB5_SEM2_DIALOG_H
+
+#include <string.h>
+
+#include "Graph.h"
+#include "Tree.h"
+#include "Func.h"
+
+int D_Add(Graph* graph);
+
+#endif //LAB5_SEM2_DIALOG_H
